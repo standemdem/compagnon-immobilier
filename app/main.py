9bd2d4b717ad14f9ee0e73a287e5951e66a59c05
YlanF <103383798+YlanF@users.noreply.github.com>
2026-08-20 11:09:30 +0200

    Nouvelle base saine

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from schemas import InputData
 from model_loader import get_model
 from security import verify_api_key
+
 import pandas as pd
 
 app = FastAPI()
@@ -14,6 +15,6 @@ def root():
 
 @app.post("/predict")
 def predict(data: InputData, api_key: str = Depends(verify_api_key)):
-    df = pd.DataFrame([data.dict()])
+    df = pd.DataFrame([data.model_dump()()])
     prediction = model.predict(df)[0]
     return {"prix_m2": prediction}
