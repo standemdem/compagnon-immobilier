3624da505c6acee98aa3f936c88464c434fcd654
YlanF <103383798+YlanF@users.noreply.github.com>
2026-08-25 10:40:29 +0200

    Rename Branche

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 import joblib
 
+MODEL_PATH = Path(__file__).resolve().parent.parent / "model" / "model.joblib"
+
 def get_model():
-    return joblib.load("model/model.joblib")
+    return joblib.load(MODEL_PATH) 
